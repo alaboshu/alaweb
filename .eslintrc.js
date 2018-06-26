ff87bfbd69ebb17eadb5f1c19f104815fe5c7b6e
sampsong <sampsong@qq.com>
2018-06-26 20:25:05 +0800

    更新eslint配置

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true
+  },
+  extends: ['plugin:vue/essential', 'standard'],
+  plugins: ['vue'],
+  rules: {
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // 'no-tabs': 0,
+    indent: 0,
+    'no-multiple-empty-lines': 0,
+
+    //  要求使用 === 和 !== (eqeqeq)  否则错误
+    eqeqeq: 2,
+    //  要求或禁止使用分号代替 ASI (semi)
+    semi: 2,
+    'no-labels': 0,
+    'space-unary-ops': [0, { words: true, nonwords: false }]
+  }
+}
--- src/router/index.js
+++ src/router/index.js
@@ -2,17 +2,25 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Chat from '../pages/chat/index.vue'
 
-
 Vue.use(Router)
 
 export default new Router({
-  routes: [{
-    path: '/',
-    name: 'index',
-    component: Chat,
-  }, {
-    path: '/test',
-    name: 'test',
-    component: '../pages/test1.vue'
-  }]
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: Chat
+    },
+    {
+      path: '/test',
+      meta: {
+        title: '会员中心',
+        login: true
+      },
+      name: 'default',
+      component: function(resolve) {
+        require(['../pages/test1.vue'], resolve)
+      }
+    }
+  ]
 })
