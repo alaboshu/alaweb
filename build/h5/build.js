59f10f46982bc04e37b8ef8fbfdbc0560663558f
sampsong <sampsong@qq.com>
2018-06-27 22:45:25 +0800

    重新打包发布

--- build/h5/build.js
+++ build/h5/build.js
@@ -1,28 +1,30 @@
-console.info('小程序运行....')
+'use strict'
+console.info('开始h5编译')
 require('./check-versions')()
 
 process.env.NODE_ENV = 'production'
 
-var ora = require('ora')
-var rm = require('rimraf')
-var path = require('path')
-var chalk = require('chalk')
-var webpack = require('webpack')
-var config = require('../config')
-var webpackConfig = require('./webpack.pro.conf')
+const ora = require('ora')
+const rm = require('rimraf')
+const path = require('path')
+const chalk = require('chalk')
+const webpack = require('webpack')
 
-var spinner = ora('building for production...')
+const config = require('./config/index')
+const webpackConfig = require('./webpack.prod.conf')
+
+const spinner = ora('building for production...')
 spinner.start()
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
   if (err) throw err
-  webpack(webpackConfig, function (err, stats) {
+  webpack(webpackConfig, (err, stats) => {
     spinner.stop()
     if (err) throw err
     process.stdout.write(stats.toString({
       colors: true,
       modules: false,
-      children: false,
+      children: false, // If you are using ts-loader, setting this to true will make TypeScript errors show up during build.
       chunks: false,
       chunkModules: false
     }) + '\n\n')
--- src/mainH5.js
+++ src/mainH5.js
@@ -2,39 +2,16 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './AppH5'
-import router from './router'
-import wxService from './js/api/wxService'
-import httpService from './js/api/httpService'
-import weui from './assets/weui/weui.less'
-
-
+// import router from './router'
+// import wxService from './js/api/wxService'
+// import httpService from './js/api/httpService'
+// import weui from './assets/weui/weui.less'
 Vue.config.productionTip = false
-Vue.use(weui)
-
-Vue.mixin({
-  data() {
-    return {
-      service: '',
-      router: '',
-      imgSrc: ''
-    }
-  },
-  created() {
-    if (window) {
-      console.log('chrome')
-      this.service = httpService
-      this.router = '/#'
-      this.imgSrc = '../..'
-    } else {
-      console.log('wx')
-      this.service = wxService
-      this.imgSrc = '/static'
-    }
-  }
-})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
+  components: {
+    App
+  }
 })
