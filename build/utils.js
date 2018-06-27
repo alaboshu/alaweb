2050eec35c9134d661c12266fa2cf73b62750c7c
F-loat <945852046@qq.com>
2018-06-28 07:37:46 +0800

    添加 wxss 处理配置

--- build/utils.js
+++ build/utils.js
@@ -62,6 +62,7 @@ exports.cssLoaders = function (options) {
   // https://vue-loader.vuejs.org/en/configurations/extract-css.html
   return {
     css: generateLoaders(),
+    wxss: generateLoaders(),
     postcss: generateLoaders(),
     less: generateLoaders('less'),
     sass: generateLoaders('sass', { indentedSyntax: true }),
