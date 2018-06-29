cb8d846e07e63c271c9107e1a51923689e4088f6
hui <2372849697@qq.com>
2018-06-29 15:16:39 +0800

    1

--- src/mainH5.js
+++ src/mainH5.js
@@ -3,8 +3,23 @@ import App from './AppH5'
 import router from './service/router'
 import weui from './assets/style/h5/weui.less'
 
+import XHeader from '@/element/common/x-header'
+import XTabbar from '@/element/common/x-tabbar'
+import XTabbarItem from '@/element/common/x-tabbar-item'
 import XGrid from '@/element/common/x-grid'
+import XGridItem from '@/element/common/x-grid-item'
+import XCell from '@/element/common/x-cell'
+import XGroup from '@/element/common/x-group'
+import XSearch from '@/element/common/x-search'
+
+Vue.component('x-header', XHeader)
+Vue.component('x-tabbar', XTabbar)
+Vue.component('x-tabbar-item', XTabbarItem)
 Vue.component('x-grid', XGrid)
+Vue.component('x-grid-item', XGridItem)
+Vue.component('x-cell', XCell)
+Vue.component('x-group', XGroup)
+Vue.component('x-search', XSearch)
 
 Vue.config.productionTip = false
 Vue.use(weui)
--- src/pages/chat/index.vue
+++ src/pages/chat/index.vue
@@ -1,14 +1,62 @@
 <template>
   <div class="page">
+    <x-header title="头部"></x-header>
     <div class="page button js_show">
       <div class="page__hd">
         <h1 class="page__title">Button</h1>
         <p class="page__desc">按钮</p>
       </div>
       <div class="page__bd page__bd_spacing">
-        <x-grid>阿斯顿发送到</x-grid>
+
+        <x-grid>
+          <x-grid-item to="/baidu">
+            <img src="" alt="" slot="icon">
+            <span slot="label">x-grid-item</span>
+          </x-grid-item>
+          <x-grid-item to="/baidu">
+            <img src="" alt="" slot="icon">
+            <span slot="label">x-grid-item</span>
+          </x-grid-item>
+          <x-grid-item to="/baidu">
+            <img src="" alt="" slot="icon">
+            <span slot="label">x-grid-item</span>
+          </x-grid-item>
+          <x-grid-item to="/baidu">
+            <img src="" alt="" slot="icon">
+            <span slot="label">x-grid-item</span>
+          </x-grid-item>
+        </x-grid>
+        <x-group title="Group+Cell">
+          <x-cell title="cell标题" value="cell说明文字" to="/baidu"></x-cell>
+        </x-group>
       </div>
     </div>
+    <x-tabbar>
+      <x-tabbar-item to="/baidu" isOn>
+        <span slot="icon" style="display: inline-block; position: relative;">
+          <img class="weui-tabbar__icon" src="" slot="icon">
+        </span>
+        x-tabbar-item
+      </x-tabbar-item>
+      <x-tabbar-item to="/baidu">
+        <span slot="icon" style="display: inline-block; position: relative;">
+          <img class="weui-tabbar__icon" src="" slot="icon">
+        </span>
+        x-tabbar-item
+      </x-tabbar-item>
+      <x-tabbar-item to="/baidu">
+        <span slot="icon" style="display: inline-block; position: relative;">
+          <img class="weui-tabbar__icon" src="" slot="icon">
+        </span>
+        x-tabbar-item
+      </x-tabbar-item>
+      <x-tabbar-item to="/baidu">
+        <span slot="icon" style="display: inline-block; position: relative;">
+          <img class="weui-tabbar__icon" src="" slot="icon">
+        </span>
+        x-tabbar-item
+      </x-tabbar-item>
+    </x-tabbar>
   </div>
 </template>
 
