55fb9cf51990009e49c4927a0ff3a688006d588f
sampsong <sampsong@qq.com>
2018-06-29 23:19:51 +0800

    重新整理h5

--- src/_start/min/main.js
+++ src/_start/min/main.js
@@ -1,18 +1,20 @@
 import Vue from 'vue'
 import MpvueRouterPatch from 'mpvue-router-patch'
-import elements from '@/elements/min/index.js'
+import weui from '@/assets/style/min/weui.less'
 import App from './App'
 import store from '@/store'
+import XButton from '@/elements/min/x-button' // 按钮
+import XPicker from '@/elements/min/x-picker' // 选择器
 
+Vue.component('x-button', XButton)
+Vue.component('x-picker', XPicker)
+
+Vue.use(MpvueRouterPatch)
+Vue.use(weui)
 Vue.config.productionTip = false
 App.store = store
 
-Vue.use(MpvueRouterPatch)
-
-const app = new Vue({
-  App,
-  elements
-})
+const app = new Vue(App)
 app.$mount()
 
 export default {
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="container">
+    <div class="weui-grids">
+      <block v-for="item in grids" :key="index">
+        <a url="" class="weui-grid" hover-class="weui-grid_active">
+          <image class="weui-grid__icon" :src="item.src" />
+          <div class="weui-grid__label">{{item.name}}</div>
+        </a>
+      </block>
+    </div>
+
     <x-picker :range="array">
-      <button type="primary">x-picker</button>
+      <x-button type="primary">x-picker</x-button>
     </x-picker>
   </div>
 </template>
@@ -10,7 +19,18 @@
   export default {
     data () {
       return {
-        array: ['美国', '中国', '巴西', '日本']
+        array: ['美国', '中国', '巴西', '日本'],
+        grids: [
+          { src: '/static/images/icon_nav_button.png', name: 'Button' },
+          { src: '/static/images/icon_nav_cell.png', name: 'Cell' },
+          { src: '/static/images/icon_nav_toast.png', name: 'Toast' },
+          { src: '/static/images/icon_nav_dialog.png', name: 'Dialog' },
+          { src: '/static/images/icon_nav_button.png', name: 'Progress' },
+          { src: '/static/images/icon_nav_panel.png', name: 'Msg' },
+          { src: '/static/images/icon_nav_article.png', name: 'Article' },
+          { src: '/static/images/icon_nav_actionSheet.png', name: 'ActionSheet' },
+          { src: '/static/images/icon_nav_icons.png', name: 'Icons' }
+        ]
       }
     },
     methods: {
