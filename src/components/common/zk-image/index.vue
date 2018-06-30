66cbc5878a0e2ab62b567bbadaaa0ce9155e4480
sampsong <sampsong@qq.com>
2018-07-01 05:01:09 +0800

    re:重新整理CSS变量

--- src/components/common/zk-image/index.vue
+++ src/components/common/zk-image/index.vue
@@ -1,35 +1,37 @@
 <template>
-  <div class="test" :style="styles">>{{viewModel}}</div>
+  <div class="zk-image" :style="styles">
+    <div class="">{{viewModel}}</div>
+  </div>
 </template>
 
 <script>
-  // import { DIY_GETLINK } from '@/service/api/apiUrl' // 引入Api接口常量
+  import { DIY_GETLINK } from '@/service/api/apiUrl' // 引入Api接口常量
   import { editSetting } from './property'
   export default {
     name: editSetting.key,
     data () {
       return {
         viewModel: '', // 数据模型
-        styles: {}
+        styles: {} // 可视化编辑样式
       }
     },
     mounted () {
-      this.init() // 组件初始化
+      this.init()
     },
     methods: {
       async  init () {
-        // var para = {
-        //   diyKey: 'grid_index'
-        // }
-        this.viewModel = await this.$api.get('api/user/info?id=1')
+        var para = {
+          diyKey: 'grid_index'
+        }
+        this.viewModel = await this.$api.get(DIY_GETLINK, para)
       }
     }
   }
 </script>
 
 <style scoped lang="less">
-  @import '~_style/index.less'; // 颜色、边框、大小请尽可能的使用变量
-  .test {
-    font-size: @brand;
+  @import '~_style/index.less'; // 颜色、边框、大小请使用assets/style/variable.less 和theme.less中的变量
+  .zk-image {
+    font-size: @font-size-base;
   }
 </style>
