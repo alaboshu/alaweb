5f37df4df5ddfde3e1b64b4ba47b69b661a2c9af
sampsong <sampsong@qq.com>
2018-07-01 06:17:16 +0800

    fix:升级包引用

---
+++ src/pages/user/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <img class="child-image" src="http://admin.czhait.com/wwwroot/assets/svg/zk-classification.svg"></img>
+    <x-icon name="zk-classification" link='/index'></x-icon>
+    <zk-grid></zk-grid>
+
+  </div>
+</template>
+
+<script>
+  // import api from '@/service/api/api'
+
+  export default {
+    config: {
+      title: '会员中心'
+    },
+    data () {
+      return {
+        clientType: ''
+      };
+    },
+    mounted () {
+      this.ApiGet()
+    },
+    methods: {
+      async ApiGet () {
+        // this.clientType = this.clientType()
+        // var data = api.getNewsList()
+        // console.info('fly', data)
+      }
+    }
+  }
+</script>
+
+<style scoped lang="less">
+</style>
