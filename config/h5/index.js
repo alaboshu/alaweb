9a412c3dbd29635ad8a99fd484d45f598b659efb
F-loat <945852046@qq.com>
2018-07-02 21:43:51 +0800

    fix: 网页打包配置

--- config/h5/index.js
+++ config/h5/index.js
@@ -45,7 +45,7 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../../dist/h5/index.html'),
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../../dist/h5'),
