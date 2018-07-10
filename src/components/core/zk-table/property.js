2a80e70484bfa95ec7310cfd1b20e0cf57ab7ba1
sampsong <sampsong@qq.com>
2018-07-10 15:20:34 +0800

    更新表格插件

---
+++ src/components/core/zk-table/property.js
@@ -0,0 +1,23 @@
+export const editSetting = {
+  'Name': 'zk-table',
+  'Path': null,
+  'Key': 'zk-table',
+  'Editors': [{
+    'GroupName': '基本设置',
+    'Items': [{
+      'Field': 'font_color',
+      'Name': '字体颜色',
+      'Type': 51,
+      'Value': '#000000',
+      'Data': null
+    }]
+  }],
+  'Events': [{
+    'Type': 3,
+    'Name': '初始化'
+  }],
+  'Type': 0,
+  'EventName': null,
+  'Id': '5b445dbd15b52f4ba8b04422',
+  'CreateTime': '2018-07-10 15:18:21'
+}
--- src/elements/common/x-grid/index.vue
+++ src/elements/common/x-grid/index.vue
@@ -35,19 +35,19 @@
 <style scoped lang="less">
   @import '~_style/index.less'; // 颜色、边框、大小请尽可能的使用变量
   .weui-grid {
-    text-decoration: none;
-    color: @brand;
-    width: 25%;
-    padding: 10px 10px;
-    .weui-grid__icon {
-      margin: 0 auto;
-    }
+  	text-decoration: none;
+  	color: @brand;
+  	width: 25%;
+  	padding: 10px 10px;
+  	.weui-grid__icon {
+  		margin: 0 auto;
+  	}
   }
   .weui-cell:visited {
-    color: #000;
+  	color: #000;
   }
   .weui-grid__label {
-    text-decoration: none;
-    color: #000;
+  	text-decoration: none;
+  	color: #000;
   }
 </style>
