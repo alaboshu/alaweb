2f5e219ba3178b23f8312a8d6d05538190db5758
sampsong <sampsong@qq.com>
2018-07-11 22:06:04 +0800

    新增富文本、Html、Text等组件

---
+++ src/components/core/zk-video/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="zk-video" :style="styles" component-path="core/zk-video">
+    <div class="">{{viewModel}}</div>
+  </div>
+</template>
+
+<script>
+  import { DIY_GETLINK_GET } from '@/service/api/apiUrl' // 引入Api接口常量
+  import { editSetting } from './property'
+  export default {
+    name: editSetting.key,
+    props: ['widgetDataId'],
+    data () {
+      return {
+        viewModel: '', // 数据模型
+        styles: {} // 可视化编辑样式
+      }
+    },
+    mounted () {
+      this.init()
+    },
+    methods: {
+      async  init () {
+        this.viewModel = await this.$api.get(DIY_GETLINK_GET, this.widgetDataId)
+      }
+    }
+  }
+</script>
+
+<style scoped lang="less">
+  @import '~_style/index.less'; // 颜色、边框、大小请使用assets/style/variable.less 和theme.less中的变量
+  .zk-video {
+  	font-size: @font-size-base;
+  }
+</style>
+
---
+++ src/components/core/zk-video/property.js
@@ -0,0 +1,23 @@
+export const editSetting = {
+  'Name': 'zk-video',
+  'Path': null,
+  'Key': 'zk-video',
+  'Editors': [{
+    'GroupName': '基本设置',
+    'Items': [{
+      'Field': 'font_color',
+      'Name': '字体颜色',
+      'Type': 51,
+      'Value': '#000000',
+      'Data': null
+    }]
+  }],
+  'Events': [{
+    'Type': 3,
+    'Name': '初始化'
+  }],
+  'Type': 0,
+  'EventName': null,
+  'Id': '5b460df3c24ec639a045ab67',
+  'CreateTime': '2018-07-11 22:02:27'
+}
