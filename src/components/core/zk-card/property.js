f0819c8aba4bedd5198f18050e43b5d6a38069ab
zhongku <2839018@qq.com>
2018-07-11 22:27:10 +0800

    更新zk-card组件

---
+++ src/components/core/zk-card/property.js
@@ -0,0 +1,23 @@
+export const editSetting = {
+  'Name': 'zk-card',
+  'Path': null,
+  'Key': 'zk-card',
+  'Editors': [{
+    'GroupName': '基本设置',
+    'Items': [{
+      'Field': 'font_color',
+      'Name': '字体颜色',
+      'Type': 51,
+      'Value': '#000000',
+      'Data': null
+    }]
+  }],
+  'Events': [{
+    'Type': 3,
+    'Name': '初始化'
+  }],
+  'Type': 0,
+  'EventName': null,
+  'Id': '5b460feca823b94b043ea18f',
+  'CreateTime': '2018-07-11 22:10:52'
+}
