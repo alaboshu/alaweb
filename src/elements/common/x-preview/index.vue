413dcafa59a96e83a3cf22ade5096b87cc87a129
zhongku <2839018@qq.com>
2018-07-20 07:14:32 +0800

    修改x-preview

--- src/elements/common/x-preview/index.vue
+++ src/elements/common/x-preview/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="weui-form-preview">
+  <div class="common-x-preview  weui-form-preview">
     <div class="weui-form-preview__hd" v-for="(item, index) in elementData" :key="index" v-if="index==0">
       <label class="weui-form-preview__label">{{ item.name }}</label>
       <em class="weui-form-preview__value">{{ item.value }}</em>
@@ -17,14 +17,24 @@
   export default {
     name: 'x-preview',
     props: {
-      elementData: {},
-      buttons: {
-        type: Array,
-        default: () => []
+      elementData: {}
+    },
+    data () {
+      return {
+      }
+    },
+    mounted () {
+      this.init()
+    },
+    methods: {
+      async init () {
       }
     }
   }
 </script>
-
 <style scoped lang="less">
+  @import '~_style/index.less';
+  .common-x-preview {
+  	font-size: @font-size-base;
+  }
 </style>
