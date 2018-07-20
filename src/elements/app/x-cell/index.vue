d813288c5e835ba05827d210d0ea717188d1aa47
zhongku <2839018@qq.com>
2018-07-20 11:19:05 +0800

    更新模板

---
+++ src/elements/app/x-cell/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="app-x-cell  weui-form-preview" element-path="app/x-cell">
+  {{elementData}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'x-cell',
+    props: {
+      elementData: {}
+    },
+    data () {
+      return {
+      }
+    },
+    mounted () {
+      this.init()
+    },
+    methods: {
+      async init () {
+      }
+    }
+  }
+</script>
+<style scoped lang="less">
+  @import '~_style/index.less';
+  .app-x-cell {
+  	font-size: @font-size-base;
+  }
+</style>
---
+++ src/elements/pc/x-swiper/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="pc-x-swiper  weui-form-preview" element-path="pc/x-swiper">
+  {{elementData}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'x-swiper',
+    props: {
+      elementData: {}
+    },
+    data () {
+      return {
+      }
+    },
+    mounted () {
+      this.init()
+    },
+    methods: {
+      async init () {
+      }
+    }
+  }
+</script>
+<style scoped lang="less">
+  @import '~_style/index.less';
+  .pc-x-swiper {
+  	font-size: @font-size-base;
+  }
+</style>
