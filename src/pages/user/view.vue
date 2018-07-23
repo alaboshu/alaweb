1d882629b0a3f0b8d60bd8ad5861b854524ea62a
zhongku <2839018@qq.com>
2018-07-23 11:55:41 +0800

    gengxin

--- src/pages/user/view.vue
+++ src/pages/user/view.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="pages-user-view">
-    <!-- <zk-head backText="首页" :title="pageInfo.title" v-if="asyncFlag"></zk-head> -->
+    <zk-head backText="首页" :title="pageInfo.title" v-if="asyncFlag"></zk-head>
     <zk-preview v-if="asyncFlag" :widget="widget" :previewId="previewId"></zk-preview>
     <zk-foot></zk-foot>
   </div>
 </template>
 
 <script>
-  // import { THEME_GETPAGE_GET } from '@/service/api/apiUrl'
+  import { THEME_GETPAGE_GET } from '@/service/api/apiUrl'
   export default {
     config: {
       'navigationBarTitleText': '会员详情'
@@ -24,7 +24,7 @@
     methods: {
       async init () {
         this.$loading = true
-        // this.pageInfo = await this.$api.get(THEME_GETPAGE_GET, 'clientType=' + this.$client + '&url=' + this.$route.path)
+        this.pageInfo = await this.$api.get(THEME_GETPAGE_GET, 'clientType=' + this.$client + '&url=' + this.$route.path)
         this.$loading = false
         this.widget = {
           apiUrl: 'Api/User/Preview'
