88ab4ad415bf24dcfa6e4363022c8e3ea3d76152
zhongku <2839018@qq.com>
2018-07-23 22:13:07 +0800

    新增app配置

---
+++ src/config/apis.js
@@ -0,0 +1,3 @@
+export default {
+  'COMMON.getInfo': '/test/info'
+}
---
+++ src/pages/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <text class="title">Hello Weex !</text>
+    <text class="title">Hello Weex !</text>
+    <a href="pages/mediator/index.js">测试页面</a>
+    <wxc-icon name="success"></wxc-icon>
+  </div>
+</template>
+
+<script>
+
+</script>       
+
+<style scoped>
+  .title {
+  	font-size: 72px;
+  	padding: 50px;
+  	width: 750px;
+  	color: #309bf8;
+  	text-align: center;
+  }
+</style>
