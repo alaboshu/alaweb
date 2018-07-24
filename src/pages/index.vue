96bb6ceabaec3e3650a6156ec1ae9bf9cd4b22ee
zhongku <2839018@qq.com>
2018-07-24 19:36:39 +0800

    更新app main.js

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,8 +3,6 @@
     <text class="title">zkweb !</text>
     <text class="title">宋和珈 ！</text>
     <text class="title">Hello Weex !</text>
-    <wxc-cell title="发送请求 (开发模式才有效果)" desc="$fetch 支持 restful 标准, 高度自定义，支持 cookie 等。">
-    </wxc-cell>
   </div>
 </template>
 
