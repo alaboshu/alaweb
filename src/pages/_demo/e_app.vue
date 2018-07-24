73c0a53be6b4ae9a1baa667dcc79fa317fe1cd82
zhongku <2839018@qq.com>
2018-07-24 08:27:23 +0800

    gengxinApp

--- src/pages/_demo/e_app.vue
+++ src/pages/_demo/e_app.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="pages--components">
-	
+	      <x-col></x-col>
+      <x-icon></x-icon>
+      <x-row></x-row>
+      <x-badge></x-badge>
+      <x-box></x-box>
+      <x-color></x-color>
+      <x-nodata></x-nodata>
+      <x-foot></x-foot>
+      <x-verifiy-phone></x-verifiy-phone>
+
   </div>
 </template>
 <script>
