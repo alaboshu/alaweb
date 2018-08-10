bacbe24a0682b730c56215f226f666dc7e4fd60e
zhongku <2839018@qq.com>
2018-08-11 06:57:37 +0800

    更新x-image

--- src/components/core/zk-swiper/index.vue
+++ src/components/core/zk-swiper/index.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="zk-swiper" :style="styles">
-    <x-swiper :elementData="viewModel" v-if="asyncflag"></x-swiper>
+  <div class="zk-swiper" :style="styles" component-path="core/zk-swiper" v-if="asyncflag" :data-id="dataId">
+    <x-swiper :elementData="viewModel"></x-swiper>
   </div>
 </template>
 
 <script>
-  import { THEME_GETVALUE_GET } from '@/service/api/apiUrl'
+  import { THEME_GETLINK_GET } from '@/service/api/apiUrl'
   import { editSetting } from './property'
   export default {
     name: editSetting.key,
     data () {
       return {
         viewModel: '', // 数据模型
-        styles: {}, 
+        styles: {},
         indicatorDots: true,
         autoplay: true,
         interval: 5000,
@@ -21,17 +21,18 @@
         asyncflag: false
       }
     },
-    props: ['dataId'],
+    props: {
+      dataId: {
+        type: String,
+        default: '5b406cddfef00000a0000004'
+      }
+    },
     mounted () {
       this.ApiGet()
     },
     methods: {
       async  ApiGet () {
-        const para = {
-          dataId: this.dataId,
-          defaultId: '5b406cddfef00000a0000004'
-        }
-        this.viewModel = await this.$api.get(THEME_GETVALUE_GET, para)
+        this.viewModel = await this.$api.get(THEME_GETLINK_GET, 'dataId=' + this.dataId)
         console.info('组件数据swiper', this.viewModel)
         this.asyncflag = true
       }
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import routes from './routes'
+import routes from './routers'
 
 Vue.use(Router)
 
-routes.push({ path: '/', redirect: '/pages/index' })
+
 
 export default new Router({
   routes,
