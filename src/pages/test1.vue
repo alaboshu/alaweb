605f2bb904fdb4636a8063a08b1108a6001c8ae1
zhongku <2839018@qq.com>
2018-08-14 17:53:58 +0800

    gengxin

--- src/pages/test1.vue
+++ src/pages/test1.vue
@@ -1,26 +1,12 @@
 <template>
-  <div>
-    <div v-for="(widget,index) in pageInfo" :key="index">
-      <zk-head :widget="widget" v-if="widget.path==='theme/zk-head'"></zk-head>
-      <zk-swiper :widget="widget" v-if="widget.path==='theme/zk-swiper'"></zk-swiper>
-      <zk-grid :widget="widget" v-if="widget.path==='theme/zk-grid'"></zk-grid>
-      <zk-image :widget="widget" v-if="widget.path==='theme/zk-image'"></zk-image>
-      <zk-top-nav :widget="widget" v-if="widget.path==='theme/top-nav'"></zk-top-nav>
-      <zk-foot :widget="widget" v-if="widget.path==='theme/zk-foot'"></zk-foot>
-    </div>
-  </div>
+  <zk-page :pageinfo="pageInfo"></zk-page>
 </template>
 
 <script>
-  import { THEME_GETPAGE_GET } from '@/service/api/apiUrl'
   export default {
-    config: {
-      'navigationBarTitleText': '测试一'
-    },
     data () {
       return {
-        pageInfo: '',
-        asyncFlag: false
+        pageInfo: ''
       }
     },
     mounted () {
@@ -28,19 +14,12 @@
     },
     methods: {
       async init () {
-        this.$loading = true
-        this.pageInfo = await this.$api.get(THEME_GETPAGE_GET, 'clientType=' + this.$client + '&url=' + this.$route.path)
-        this.$loading = false
-        this.asyncFlag = true
-        // console.info('测试一页面', this.pageInfo)
+        this.pageInfo = await this.$themeApi.pageInfo(this.$client, this.$route.path)
       }
     }
   }
 </script>
-
-<style scoped lang="less">
-  .pages--test1 {
-    width: 100%;
-  }
+<style >
 </style>
+
 
