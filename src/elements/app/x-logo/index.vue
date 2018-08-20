8457fae743c570b1432a2e4ee4920ef426f7cd7f
zhongku <2839018@qq.com>
2018-08-20 11:50:11 +0800

    修改main.js

---
+++ src/elements/app/x-logo/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="app-x-logo" element-path="app/x-logo">
+  {{viewModel}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'x-logo',
+    props: {
+      elementData: {}
+    },
+    data () {
+      return {
+        viewModel: ''
+      }
+    },
+    mounted () {
+      this.init()
+    },
+    methods: {
+      async init () {
+        if (this.elementData !== undefined) {
+          this.viewModel = this.elementData
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped lang="less">
+  @import '~_style/index.less';
+  .app-x-logo {
+  	font-size: @font-size-base;
+  }
+</style>
