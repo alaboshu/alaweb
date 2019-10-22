38763a03f13188d280b47c6dedc89a83bab162db
2839018@qq.com <2839018@qq.com>
2019-10-22 16:29:33 +0800

    fex：x-password

--- src/elements/all/x-password/index.vue
+++ src/elements/all/x-password/index.vue
@@ -34,15 +34,6 @@
       init () {
         if (this.dataModel) {
           this.viewModel = this.dataModel
-        },
-        inputChange() { }
-      },
-      isNumber () {
-        if (this.isNum === true) {
-          var rep = /^[0-9]*$/
-          if (rep.test(this.viewModel) === true) {
-            return 6
-          }
         }
       },
       isNumber (type = false) {
