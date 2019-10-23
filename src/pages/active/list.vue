0f13389b92c6aad738949bd0f772eeee7385fa72
zkcloud <asd@qq.com>
2019-10-23 16:25:33 +0800

    fix:list

---
+++ src/pages/active/list.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <zk-head title="会员激活管理" :showHead="true"></zk-head>
+    <zk-list type="UserLevelActiveForm"></zk-list>
+  </div>
+</template>
+<script>
+  export default {
+  }
+</script>
+<style lang="scss" scoped>
+</style>
