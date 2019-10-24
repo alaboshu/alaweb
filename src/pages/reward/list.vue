5950f360ecf46d4d31ca9a12ab85c4c23bd57616
zkcloud <asd@qq.com>
2019-10-24 08:58:44 +0800

    fix：user index

---
+++ src/pages/reward/list.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <zk-head title="分润中心" :showHead="true"></zk-head>
+    <zk-list type="RewardAutoList"></zk-list>
+  </div>
+</template>
+<script>
+  export default {
+  }
+</script>
+<style lang="scss" scoped>
+</style>
