c2490a4cc84f28862acf09c0cfbd3c3c98ec978a
zkcloud <asd@qq.com>
2019-10-24 09:16:55 +0800

    fix:财务明细

---
+++ src/pages/asset/bill.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <zk-head title="财务明细" :showHead="true"></zk-head>
+    <zk-list type="BillAutoList"></zk-list>
+  </div>
+</template>
+<script>
+  export default {
+  }
+</script>
+<style lang="scss" scoped>
+</style>
