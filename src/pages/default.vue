db026a898579efd2d544004d6769103d59c3a421
alabo <alabo@qq.com>
2019-11-05 08:08:48 +0800

    fix:update delete

--- src/pages/default.vue
+++ src/pages/default.vue
@@ -1,32 +1,15 @@
 <template>
   <view>
-    <zk-index-swiper :widget="indexSwiperData" style="background: #F0F0F0;"></zk-index-swiper>
-    <zk-parting-line style="background: #F0F0F0;"></zk-parting-line>
-    <zk-index-new-top :widget="indexNewData" style="background: #f0f0f0;"></zk-index-new-top>
-    <zk-image :widget="imageData" style="background: #f0f0f0;"></zk-image>
-    <zk-border-header :widget="indexNewData" type="2" style="background: #f0f0f0;"></zk-border-header>
-    <zk-index-hot-shop :widget="hotShopData" style="background: #f0f0f0;"></zk-index-hot-shop>
-    <zk-index-day-update :widget="dayUpdateData"></zk-index-day-update>
-    <!-- <zk-index-type-list :widget="typeListData"></zk-index-type-list> -->
+
   </view>
 </template>
 
 <script>
-  import indexSwiperJson from './json/indexSwiper.json'
-  import indexNewJson from './json/indexNewTop.json'
-  import imageJson from './json/imageList.json'
-  import dayUpdateJson from './json/dayUpdate.json'
-  import hotShopJson from './json/hotShop.json'
-  import typeListJson from './json/typeList.json'
+
   export default {
     data () {
       return {
-        indexSwiperData: indexSwiperJson,
-        indexNewData: indexNewJson,
-        imageData: imageJson,
-        dayUpdateData: dayUpdateJson,
-        hotShopData: hotShopJson,
-        typeListData: typeListJson
+
       }
     }
   }
