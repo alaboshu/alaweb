3482b57e6c70d72db87cb40fcdcf7daa5f0bced9
Administrator <Administrator@DESKTOP-JD8QDF1>
2019-11-13 14:56:40 +0800

    fix:修改名称

--- src/pages/mix/wedget.js
+++ src/pages/mix/wedget.js
@@ -1,10 +1,13 @@
 import theme from '@/service/all/theme'
 export default {
-  onLoad(option) {
+  onLoad (option) {
     this.option = option
     console.info('options', option)
   },
   methods: {
-    async init() {}
+    async init () {
+      this.viewModel = await this.$api.themePage(this.option)
+      console.info('this.viewMode;l', this.viewModel)
+    }
   }
 }
