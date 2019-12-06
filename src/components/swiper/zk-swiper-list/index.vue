c99910f30ac90783a4e777115080890ac440d4b6
Administrator <Administrator@DESKTOP-JD8QDF1>
2019-12-06 16:43:19 +0800

    fix:zk-swiper-list

--- src/components/swiper/zk-swiper-list/index.vue
+++ src/components/swiper/zk-swiper-list/index.vue
@@ -2,6 +2,7 @@
   <view v-if="swiperModel">
     <swiper-item-1 v-if="swiperModel.swiperType == 1" :swiperModel="swiperModel.swiperList"></swiper-item-1>
     <swiper-item-2 v-if="swiperModel.swiperType == 2" :swiperModel="swiperModel.swiperList"></swiper-item-2>
+    <swiper-item-3 v-if="swiperModel.swiperType == 3" :swiperModel="swiperModel.swiperList"></swiper-item-3>
   </view>
 </template>
 
--- src/components/swiper/zk-swiper-list/styles/style-mix.js
+++ src/components/swiper/zk-swiper-list/styles/style-mix.js
@@ -1,8 +1,10 @@
 import swiperItem1 from './swiper-item-1'
 import swiperItem2 from './swiper-item-2'
+import swiperItem3 from './swiper-item-3'
 export default {
   components: {
     swiperItem1,
-    swiperItem2
+    swiperItem2,
+    swiperItem3
   }
 }
