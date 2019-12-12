763f43c6d74835c6dc7fdbcd8f7fbab9310b9079
sampsong <2839018@qq.com>
2019-12-12 22:42:11 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -36,7 +36,6 @@ import zkOrderList from '@/components/order/zk-order-list/index.vue'
 import zkOrderRefund from '@/components/order/zk-order-refund/index.vue'
 import zkOrderShow from '@/components/order/zk-order-show/index.vue'
 import zkProductClass from '@/components/product/zk-product-class/index.vue'
-import zkProductItem from '@/components/product/zk-product-item/index.vue'
 import zkProductList from '@/components/product/zk-product-list/index.vue'
 import zkProductListpage from '@/components/product/zk-product-listpage/index.vue'
 import zkProductShowpage from '@/components/product/zk-product-showpage/index.vue'
@@ -98,7 +97,6 @@ Vue.component('zk-order-list', zkOrderList)
 Vue.component('zk-order-refund', zkOrderRefund)
 Vue.component('zk-order-show', zkOrderShow)
 Vue.component('zk-product-class', zkProductClass)
-Vue.component('zk-product-item', zkProductItem)
 Vue.component('zk-product-list', zkProductList)
 Vue.component('zk-product-listpage', zkProductListpage)
 Vue.component('zk-product-showpage', zkProductShowpage)
