7c52cfafec45999b64e13afa43c7b85631b09b21
Administrator <Administrator@DESKTOP-JD8QDF1>
2019-12-13 11:46:18 +0800

    fix:修复下拉刷新

--- src/elements/x-widget/service.js
+++ src/elements/x-widget/service.js
@@ -1,13 +1,12 @@
-import Vue from 'vue'
 export default {
   // 下拉刷新，只在最后一个是列表时触发
-  scrollChange () {
-    if (Vue.prototype.$refs.scrollItem) {
-      var scrollItem = Vue.prototype.$refs.scrollItem
+  scrollChange (jsThis) {
+    if (jsThis.$refs.scrollItem) {
+      var scrollItem = jsThis.$refs.scrollItem
       if (scrollItem.$refs.widgetItem) {
         var swiperWidget = scrollItem.$refs.widgetItem[scrollItem.$refs.widgetItem.length - 1]
         if (swiperWidget.widget.name === 'zk-product-list') {
-          swiperWidget.$refs.zkProductList.scrollView()
+          swiperWidget.$children[0].$children[0].scrollView()
         }
       }
     }
