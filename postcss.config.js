01e7b5631898a467a7d261a82a00a64cdb95990f
Administrator <Administrator@DESKTOP-JD8QDF1>
2019-12-14 09:51:22 +0800

    fix:修复运行命令报错， postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ const pkg = require('./package.json')
 module.exports = {
   plugins: [
     require('autoprefixer')({
-      browsers: pkg.browserslist
+      overrideBrowserslist: pkg.browserslist
     }),
     require('@dcloudio/vue-cli-plugin-uni/packages/postcss')
   ]
