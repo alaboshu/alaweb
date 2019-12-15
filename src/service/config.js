41e1c53e868259e0bcc82c2fd4cd7765248e9ff8
sampsong <2839018@qq.com>
2019-12-15 10:53:19 +0800

    fix: order by user info

--- src/service/config.js
+++ src/service/config.js
@@ -1,17 +1,17 @@
-  //  const apiBaseUrl = 'http://localhost:1800/'
-  const apiBaseUrl = 'http://retail_v13.api.5ug.com/'
-  //  const apiBaseUrl = 'http://retail_v13.api.5ug.com/'
-  const version = '1.9.821415'
-  const isTenant = false
+    const apiBaseUrl = 'http://localhost:1800/'
+    // const apiBaseUrl = 'http://retail_v13.api.5ug.com/'
+    //  const apiBaseUrl = 'http://retail_v13.api.5ug.com/'
+    const version = '1.9.821415'
+    const isTenant = false
 
-  const key = 'LUPGHODKYFWYO0YARAWNTKMM8QZQ8L1BA7ZMLEAE53V6NQS7ZWJ1WU' // 公钥
-  const projectId = 'C94E355F-79DC-44E8-8EF1-CBF886129EDA' // 私钥ID
-  const privateKey = 'zhonghuarenmingonghegouchenglile' // 私钥
-  module.exports = {
-    apiBaseUrl,
-    projectId,
-    key,
-    isTenant,
-    privateKey,
-    version
-  }
+    const key = 'LUPGHODKYFWYO0YARAWNTKMM8QZQ8L1BA7ZMLEAE53V6NQS7ZWJ1WU' // 公钥
+    const projectId = 'C94E355F-79DC-44E8-8EF1-CBF886129EDA' // 私钥ID
+    const privateKey = 'zhonghuarenmingonghegouchenglile' // 私钥
+    module.exports = {
+      apiBaseUrl,
+      projectId,
+      key,
+      isTenant,
+      privateKey,
+      version
+    }
