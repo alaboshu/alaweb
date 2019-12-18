0d2bd5f88a57405ea120705e469e9ae7d38c3d14
Administrator <Administrator@DESKTOP-JD8QDF1>
2019-12-18 10:51:04 +0800

    fix:数据展示

--- src/components/user/zk-user-collect/index.vue
+++ src/components/user/zk-user-collect/index.vue
@@ -2,11 +2,11 @@
   <div class="zk-user-collect" v-if="collectModel">
     <view v-if="collectModel.length > 0">
       <view class="list" v-for="(item, index) in collectModel" :key="index">
-        <img src="https://s-open.qiniuniu99.com//wwwroot/uploads/api/2019-11-15/5dce1a76a55e6820f808e9dd.jpg_240X240.jpg" class="img" alt="">
+        <img :src="item.image" class="img" alt="">
         <view class="right">
-          <view class="collect-title">澳柯玛暖风机（化妆镜）NF05L939</view>
-          <view class="collect-intro">100人收藏</view>
-          <view class="collect-foot"><span class="span">￥</span>280.<span class="span">00</span></view>
+          <view class="collect-title">{{item.name}}</view>
+          <view class="collect-intro">{{item.entityId}}人收藏</view>
+          <view class="collect-foot"><span class="span">￥</span>100.<span class="span">00</span></view>
         </view>
       </view>
     </view>
